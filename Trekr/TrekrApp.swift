312387df0b39d8e167cf04df88d2cd6b2ab4400f
Robin Malhotra <malhotra.robin@yahoo.co.in>
2020-12-30 22:51:31 +0000

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Robin Malhotra on 30/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
